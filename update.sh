4bea841cc8283ad97af5702a84cce10f4bcc8897
kiryk <alkiryk@wp.pl>
2021-09-01 16:25:15 +0200

    add update.sh

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Requires hit, npm, Python 3.8 and Angular
+
+BASE=$(pwd)
+REPOS=https://github.com/ankieter-gui
+
+git clone $REPOS/engine
+git clone $REPOS/interface
+
+mv -r engine/* ./
+
+cd interface
+npm install
+ng build --configuration production
+cd $BASE
+cp -r interface/dist/frontend/* static/
+
+git add .
+git commit -am 'sync'
