7741982d8eab3ba76f2860ca61863fd8f25791a7
kiryk <alkiryk@wp.pl>
2021-09-02 09:45:37 +0200

    sync

--- update.sh
+++ update.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 # Requires hit, npm, Python 3.8 and Angular
 
+# Optional arguments: $1 -- engine branch
+#                     $2 -- interface branch
+
 BASE=$(pwd)
 REPOS=https://github.com/ankieter-gui
 
 git clone $REPOS/engine
+if [ $# -ge 3 ]; then
+	git checkout $1
+fi
+
+
 git clone $REPOS/interface
+if [ $# -ge 3 ]; then
+	git checkout $2
+fi
 
 mv engine/* .
 
@@ -16,4 +27,9 @@ cd $BASE
 cp -r interface/dist/frontend/* static/
 
 git add .
-git commit -am 'sync'
+
+if [ $# -ge 3 ]; then
+	git commit -am "sync engine: $1 interface: $2"
+else
+	git commit -am "sync"
+fi
