a0a5fa92986738ee40d762b967766b9b007af185
emil <emilm7843@gmail.com>
2021-11-14 23:27:53 +0100

    sync

--- table.py
+++ table.py
@@ -31,8 +31,26 @@ def filter_ne(c):  return lambda n: n if pandas.notna(n) and n != c     else pan
 def filter_in(*c): return lambda n: n if pandas.notna(n) and n in c     else pandas.NA
 def filter_ni(*c): return lambda n: n if pandas.notna(n) and n not in c else pandas.NA
 
+def tobasetypes(s):
+    if isinstance(s.dtype, pandas.core.arrays.string_.StringDtype):
+        s = s.astype('string')
+    if isinstance(s.dtype, pandas.core.arrays.floating.Float64Dtype):
+        s = s.astype('float')
+    if isinstance(s.dtype, pandas.core.arrays.integer.Int64Dtype):
+        s = s.astype('int')
+    return s
+
 def rows(s):  return len(s)
-def share(s): return s.value_counts().to_dict()
+def share(s):
+    s = s.value_counts().to_dict()
+
+    # Sometimes, if the original series contained a numeric type, the same type
+    # will be used for count values. Here they're converted back to ints,
+    # as the rest of the software expects.
+    for k, v in s.items():
+        s[k] = int(v)
+
+    return s
 def mode(s):
     s = s.value_counts().to_dict()
     return max(s, key=s.get)
@@ -347,7 +365,7 @@ def reorder(data):
     returns survey reordered data
     """
 
-    data = data.fillna('nd.')
+    data = data.apply(tobasetypes)
 
     data.columns = [f'{label}' for label, aggr in data.columns]
 
